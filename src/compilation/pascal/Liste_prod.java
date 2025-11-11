412748e1d80a676306f305b1b8574adb3be83126
Ronan MERIEN <rmerien@hotmail.com>
2025-11-11 16:33:55 +0100

    Init Pascal project

---
+++ src/compilation/pascal/Liste_prod.java
@@ -0,0 +1,9 @@
+package compilation.pascal;
+
+public class Liste_prod {
+   int prod_lex;     /* Numero de la production              */
+   Liste_prod suivant = null;
+
+   public Liste_prod() {
+   }
+}
